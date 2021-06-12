263a87715956bd5402c28c8ad10ef3eb5ecb8ed3
Akif Manzoor <akifmanzoor124@gmail.com>
2021-06-12 17:18:48 -0400

    clean up UI

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
     <div className="flex flex-col w-full h-full bg-landing bg-no-repeat bg-cover">
 
       <header className="h-1/3">
-        <img className="w-24 mt-4 ml-4 md:w-24 md:ml-8" src={logo} alt="" />
+        <img className="w-24 mt-6 ml-6 md:w-20 md:ml-8" src={logo} alt="" />
       </header>
 
-      <div className="flex flex-col h-2/3 mx-4 md:ml-28 md:mt-16 md:w-1/4">
-        <h1 className="text-5xl font-semibold mb-2">Connecting Chefs to Kitchen Spots</h1>
+      <div className="flex flex-col h-2/3 mx-4 md:ml-32 md:mt-16 md:w-1/4">
+        <h1 className="text-5xl font-semibold mb-4">Connecting Chefs to Kitchen Spots</h1>
         <button className="bg-red-500 rounded-full text-3xl text-white tracking-wide font-semibold p-2 mt-2" onClick={() => setTypeform(true)}>Request Access</button>
       </div>
 
